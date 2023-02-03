fb769e809cb09d2c28c4a57e3b1dc2b2ad38e8a5
Saki Salman <codersaki98@gmail.com>
2023-02-03 22:19:59 +0600

    Uploaded

---
+++ Problem 1/arrayCheck.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, 3, 4];
+
+array(arr);
+// const is_array = (array) => {
+//   let result = Array.isArray(array);
+
+//   console.log(result);
+// };
+
+function array(arr) {
+  let result = Array.isArray(arr);
+
+  console.log(result);
+}
